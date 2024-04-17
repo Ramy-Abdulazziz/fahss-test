d4f2483823a6a13abcefe0e0bbe1247b0ed5469b
Ramy <ramy@Kurama>
2024-04-17 11:33:50 -0400

    switched database for start of data collection

--- setup/settings.js
+++ setup/settings.js
@@ -1,7 +1,7 @@
 var Experigen =  {
 	settings: {
 
-		experimentName: "fahssresearcharbtest", // use only A-Z, a-z, 0-9
+		experimentName: "fahssresearcharbdatacollection", // use only A-Z, a-z, 0-9
 		
 		databaseServer: "https://sdb.phonologist.org/experigen1/",
 		recorderServer: "https://sdb.phonologist.org/audio/upload.php",
